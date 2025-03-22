ecdcae18a76a7d8ad532aac24618c019449ada89
Gabriel <gabrielantony56@gmail.com>
2025-03-22 23:41:10 +0530

    trade tool config

--- gsaw-agent/src/tools/index.js
+++ gsaw-agent/src/tools/index.js
@@ -1,5 +1,6 @@
 import { initializeSearchTool } from "./search.js";
 import { initializePriceTool } from "./price.js";
+import { initializeTradeTool } from "./trade.js";
 import { initializeTransferTool } from "./transfer.js";
 import { initializeTransactionTool } from "./transaction.js";
 
