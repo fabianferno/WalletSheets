3e793284177823839cae8e35f16cbfb9c6f252f0
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2025-03-23 11:19:27 +0530

    chore: Removed web

--- web/src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export { Button } from "./Button";
-export { default as Card } from "./Card";
-export { default as Input } from "./Input";
-export { Table, TableHead, TableBody, TableRow, TableCell } from "./Table";
-export { default as Badge } from "./Badge";
-export { default as Checkbox } from "./Checkbox";
